a9deb5f36c0b8cee5b81c2fd7a3f5d238d56aac2
David Anderson <matandersonlv@hotmail.com>
2018-06-06 23:01:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ beforeEach(function() {
   a = Math.floor(Math.random() * 1000)
   b = Math.floor(Math.random() * 1000)
 })
+
+
+function add(a, b) adds two numbers and returns the result', function() {
+  return 
+})
